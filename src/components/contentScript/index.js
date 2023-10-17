b0656bcfe3c5bd45d868654094e880b7ae626e27
RayZhou <ray.zhou@scmp.com>
2023-10-17 16:04:46 +0800

    tune webpack js injection

---
+++ src/components/contentScript/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "@webcomponents/custom-elements";
+import ContentScript from "./contentScript";
+
+class ReactExtensionContainer extends HTMLElement {
+    connectedCallback() {
+        const mountPoint = document.createElement("span");
+        mountPoint.id = "reactExtensionPoint";
+
+        const reactRoot = this.attachShadow({ mode: "open" }).appendChild(
+            mountPoint
+        );
+
+        ReactDOM.render(
+                <ContentScript />,
+            mountPoint
+        );
+    }
+}
+
+const initWebComponent = function () {
+    customElements.define("react-extension-container", ReactExtensionContainer);
+
+    const app = document.createElement("react-extension-container");
+    document.documentElement.appendChild(app);
+};
+
+initWebComponent();
---
+++ src/components/popup/Popup.js
@@ -0,0 +1,47 @@
+/* global chrome */
+
+// import logo from "./logo.svg";
+// import "./Popup.css";
+
+export const getCurrentTabUId = (callback) => {
+    const queryInfo = { active: true, currentWindow: true };
+
+    chrome.tabs &&
+        chrome.tabs.query(queryInfo, (tabs) => {
+            callback(tabs[0].id);
+        });
+};
+
+function Popup() {
+    const sendMessage = () => {
+        getCurrentTabUId((id) => {
+            id &&
+                chrome.tabs.sendMessage(id, {
+                    value: "openPopup",
+                });
+            window.close();
+        });
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                <button onClick={sendMessage}>Open popup</button>
+            </header>
+        </div>
+    );
+}
+
+export default Popup;
